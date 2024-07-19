25a903bab3a8eefd3924ec627264f30eefeca0f1
MaxwellLeh <max.lehain@gmail.com>
2024-07-19 13:46:57 +0100

    completesGameFunction

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
-document.addEventListener("", () => {
-    const square = document.querySelectorAll(".cell");
+async function initializeGame() {
 
-    square.forEach(cell => {
-        cell.addEventListener("click", () => {
-            cell.style.backgroundColor = "black";
+    const game = document.querySelectorAll(".cell");
+
+    for (let i = 0; i < game.length; i++) {
+
+        let colorState = 0;
+        const play = game[i];
+
+        play.addEventListener("click", () => {
+
+            if (colorState === 0) {
+                play.style.backgroundColor = "black";
+                colorState = 1; 
+            } else if (colorState === 1) {
+                play.style.backgroundColor = "red";
+                colorState = 2;
+            } else {
+                play.style.backgroundColor = "white";
+                colorState = 0; 
+            }
         });
-    });
-});
+    }
+}
+
+initializeGame();
